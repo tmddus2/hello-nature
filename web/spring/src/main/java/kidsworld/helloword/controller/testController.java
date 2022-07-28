f74d59f83a56ec989c6714571816f7469a60eb76
tmddus2 <tmddus2__@naver.com>
2022-07-28 21:29:29 +0900

    application.yml 추가

--- web/spring/src/main/java/kidsworld/helloword/controller/testController.java
+++ web/spring/src/main/java/kidsworld/helloword/controller/testController.java
@@ -1,2 +1,13 @@
-package kidsworld.helloword.controller;public class testController {
+package kidsworld.helloword.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello world!";
+    }
 }
