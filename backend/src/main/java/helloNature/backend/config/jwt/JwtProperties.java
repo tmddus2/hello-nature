85019fe2ad2721eaa58634e0e39dd28e97bfc01b
tmddus2 <tmddus2__@naver.com>
2022-08-06 22:04:37 +0900

    signin 기능 추가

--- backend/src/main/java/helloNature/backend/config/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package helloNature.backend.config.jwt;
-
-public interface JwtProperties {
-    String SECRET = "hello-nature";
-    int EXPIRATION_TIME = 864000000;
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
