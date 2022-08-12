42aa86266171cb2b0bdfdd8a1ed6e38013beba78
tmddus2 <tmddus2__@naver.com>
2022-08-12 13:44:29 +0900

    conflict merge

--- backend/src/main/java/helloNature/backend/controller/AuthController.java
+++ backend/src/main/java/helloNature/backend/controller/AuthController.java
@@ -27,7 +27,7 @@ public class AuthController {
     private final AuthService authService;
 
 
-    @PostMapping("/signup")
+    @PostMapping("/signup") // http://localhost:8080/api/signup
     public User signup(@RequestBody SignupDto signupDto) {
         return authService.signup(signupDto);
     }
