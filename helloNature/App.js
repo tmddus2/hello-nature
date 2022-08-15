c9eb4066f9f1105937f4ccf1f6b70b4bfbc8a2ab
tmddus2 <tmddus2__@naver.com>
2022-08-15 22:27:41 +0900

    merge합니다

--- helloNature/App.js
+++ helloNature/App.js
@@ -1,32 +1,33 @@
-import {StatusBar} from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native'
-import {createStackNavigator} from '@react-navigation/stack'
-import {NavigationContainer} from '@react-navigation/native'
+import { StyleSheet, Text, View } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import 'react-native-gesture-handler'
 
 import Home from './screens/Home'
 import Login from './screens/Login'
 
 const Stack = createStackNavigator();
 
-export default function App(){
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName = "Login" screenOptions = {{
-        headerShown:false
+      <Stack.Navigator initialRouteName="Login" screenOptions={{
+        headerShown: false
       }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name ="Login" component={Login}/>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-} 
+}
 
 const styles = StyleSheet.create({
-  container:{
-    flex : 1,
-    backgroundColor:'#fff',
-    alignItems:'center',
-    justifyContent:'center',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 })
