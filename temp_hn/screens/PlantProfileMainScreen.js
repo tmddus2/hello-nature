0bdd145ce57d78756a4e175fbfd0e309e03b6288
parkheesoo <qkrgmltn1357@naver.com>
2022-08-20 20:57:17 +0900

    로그인 회원가입 디비 연동

--- temp_hn/screens/PlantProfileMainScreen.js
+++ temp_hn/screens/PlantProfileMainScreen.js
@@ -2,7 +2,9 @@ import React, { useContext, useState } from "react";
 import { Calendar } from "react-native-calendars";
 import { format } from "date-fns";
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet,  TouchableOpacity} from 'react-native';
+
+
 
 // 머티리얼 상단 탭 내비게이터
 const Tab = createMaterialTopTabNavigator();
@@ -10,24 +12,31 @@ const Tab = createMaterialTopTabNavigator();
 
 
 function DiaryScreen({navigation}) {
-  const [selectedDate, setSelectedDate] = useState(
-    format(new Date(), "yyyy-MM-dd"),
-  );
+
+
+  const water = {key: 'water', color: 'blue', selectedDotColor: 'blue'};
+  const nutrients = {key: 'nutrients', color: 'green', selectedDotColor: 'blue'};
+
+  const [value, onChange] = useState(new Date());
+
+
 
   const posts = [
     {
       id: 1,
       title: "제목입니다.",
       contents: "내용입니다.",
-      date: "2022-08-11",
+      date: "2022-08-26",
     },
     {
       id: 2,
       title: "제목입니다.",
       contents: "내용입니다.",
-      date: "2022-08-15",
+      date: "2022-08-27",
     }
   ];
+
+
 
   const markedDates = posts.reduce((acc, current) => {
     const formattedDate = format(new Date(current.date), 'yyyy-MM-dd');
@@ -35,26 +44,44 @@ function DiaryScreen({navigation}) {
     return acc;
   }, {});
 
+
+
+  const [selectedDate, setSelectedDate] = useState(
+    format(new Date(), "yyyy-MM-dd"),
+  );
+
+
   const markedSelectedDates = {
     markedDates,
     [selectedDate]: {
       selected: true,
-      selectedDotColor: 'green',
       marked: markedDates[selectedDate]?.marked,
     }
   }
 
-
-
+  
   return (
     <View>
-      <Calendar style={styles.calendar} theme={{
-        selectedDayBackgroundColor: '#009688',
-        arrowColor: '#6E8B3D',
-        todayTextColor: 'blue',
-      }} markedDates={markedSelectedDates} onDayPress={(day) => {
-        setSelectedDate(day.dateString)
-      }}/>
+      <Calendar style={styles.calendar}
+        theme={{
+          selectedDayBackgroundColor: '#009688',
+          arrowColor: '#009688',
+          todayTextColor: '#009688',
+        }} 
+        onChange={onChange} value={value}
+        //markingType={'multi-dot'} 
+        markedDates={markedSelectedDates}
+        onDayPress={day => {
+          setSelectedDate(day.dateString)
+          console.log('selected day', day);
+        }}
+      />
+      <View style={{position:'absolute' , alignItems :'center',marginTop:500, width:'100%'}}>
+        <TouchableOpacity style = {styles.buttonStyle}>
+          <Text style={styles.buttonTextStyle}> +  오늘 기록 하기</Text>
+        </TouchableOpacity>
+      </View>
+      
     </View>
   );
 }
@@ -105,5 +132,21 @@ const styles = StyleSheet.create({
     height:350,
     borderBottomWidth: 1,
     borderBottomColor: '#e0e0e0',
-  }
+  },
+  buttonStyle:{
+    borderWidth:2,
+    borderColor:'#6E8B3D',
+    width:150,
+    height:60,
+    borderRadius:30,
+    backgroundColor: '#6E8B3D',
+    alignItems: "center",
+    justifyContent :'center',
+    marginBottom:10,
+  },
+  buttonTextStyle:{
+    color:'gray',
+    fontWeight:'bold',
+    fontSize:15,
+  },
 });
